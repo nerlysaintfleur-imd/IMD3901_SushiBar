43e2e68fc82615a25cb4440468d82440aee875dc
nerlysaintfleur <46545149+nerlysaintfleur@users.noreply.github.com>
2019-03-06 17:11:36 -0500

    Update controller-management.js

--- public/js/controller-management.js
+++ public/js/controller-management.js
@@ -3,15 +3,16 @@
 AFRAME.registerComponent('controller-management', {
     schema : {},
     init :function(){
-        const Context_AF = this;
-        Context_AF.createHands();
+        const Context_AFR = this;
+        Context_AF.createRightHand();
+
+        const Context_AFL = this;
+        Context_AF.createLeftHand();
 
         this.leftHand = document.querySelector("#lefthand");
         this.rightHand = document.querySelector("#rightHand");
 
         
-    },
-    createHands : function(){
-       
     }
+
 });
