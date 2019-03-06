4ce774af751d3790d9825fb3d0a3be796707cdd7
nerlysaintfleur <46545149+nerlysaintfleur@users.noreply.github.com>
2019-03-06 17:11:33 -0500

    Update load-environment.js

--- public/js/load-environment.js
+++ public/js/load-environment.js
@@ -12,11 +12,8 @@ AFRAME.registerComponent('load-environment', {
 
         let kitchenElem  = document.createElement('a-entity');//create new element in memory
         kitchenElem.setAttribute('obj-model',{obj:'/assets/models/Environment_v02.obj'});
-        kitchenElem.setAttribute('position',{x:0, y:0, z:0});
+        kitchenElem.setAttribute('position',{x:0, y:1, z:0});
         kitchenElem.setAttribute('material',{src:'/assets/textures/Environment_v02.mtl'});
         
-        const scene = document.querySelector('a-scene');
-        //scene.appendChild(monumentElem);                         //append cow to parent(scene)
-
     }
 });
