31fe5a68fe74570f8d27e1628c96421b0e704b70
nerlysaintfleur <46545149+nerlysaintfleur@users.noreply.github.com>
2019-03-13 17:29:57 -0400

    Update load-environment.js

--- public/js/load-environment.js
+++ public/js/load-environment.js
@@ -5,15 +5,37 @@ AFRAME.registerComponent('load-environment', {
     init :function(){
         const Context_AF = this;
         Context_AF.createKitchen();
+        Context_AF.createAvocado();
+        Context_AF.createSalmon();
+
+
         
     },
     createKitchen : function(){
         const Context_AF = this;
 
-        let kitchenElem  = document.createElement('a-entity');//create new element in memory
+        let kitchenElem  = document.createElement('a-entity');//create new element kitchen in memory
         kitchenElem.setAttribute('obj-model',{obj:'/assets/models/environment_v05.obj'});
         kitchenElem.setAttribute('position',{x:0, y:1, z:0});
-        kitchenElem.setAttribute('material',{src:'/assets/models/environment_v05.mtl'});
+        kitchenElem.setAttribute('material',{src:'/assets/textures/environment_v05.mtl'});
+        
+    },
+    createAvocado : function(){
+        const Context_AF = this;
+
+        let avocadoElem  = document.createElement('a-entity');//create new element avocado in memory
+        avocadoElem.setAttribute('obj-model',{obj:'/assets/models/avacado.obj'});
+        avocadoElem.setAttribute('position',{x:0, y:0, z:0});
+        avocadoElem.setAttribute('material',{src:'/assets/textures/avacado.mtl'});
+        
+    },
+    createSalmon : function(){
+        const Context_AF = this;
+
+        let salmonElem  = document.createElement('a-obj-model');//create new element avocado in memory
+        salmonElem.setAttribute('obj-model',{obj:'/assets/models/salmon.obj'});
+        salmonElem.setAttribute('position',{x:0, y:0, z:0});
+        salmonElem.setAttribute('material',{src:'/assets/textures/salmon.png'});
         
     }
 });
