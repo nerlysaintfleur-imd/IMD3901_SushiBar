9fbc44094d26cc6cfbc2f85eff8341afc1add726
nerlysaintfleur <46545149+nerlysaintfleur@users.noreply.github.com>
2019-03-13 14:33:07 -0400

    Update load-environment.js

--- public/js/load-environment.js
+++ public/js/load-environment.js
@@ -11,9 +11,9 @@ AFRAME.registerComponent('load-environment', {
         const Context_AF = this;
 
         let kitchenElem  = document.createElement('a-entity');//create new element in memory
-        kitchenElem.setAttribute('obj-model',{obj:'/assets/models/Environment_v02.obj'});
+        kitchenElem.setAttribute('obj-model',{obj:'/assets/models/environment_v05.obj'});
         kitchenElem.setAttribute('position',{x:0, y:1, z:0});
-        kitchenElem.setAttribute('material',{src:'/assets/textures/Environment_v02.mtl'});
+        kitchenElem.setAttribute('material',{src:'/assets/models/environment_v05.mtl'});
         
     }
 });
