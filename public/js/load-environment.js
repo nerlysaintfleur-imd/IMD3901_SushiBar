ff0d33cdfb3b3ca86ba8d7b0a0c86fd0aef7dc36
Nerly <nerlysaintFleur@cmail.carleton.ca>
2019-03-14 23:09:20 -0400

    Update load-environment.js

--- public/js/load-environment.js
+++ public/js/load-environment.js
@@ -14,19 +14,19 @@ AFRAME.registerComponent('load-environment', {
         const Context_AF = this;
 
         let kitchenElem  = document.createElement('a-entity');//create new element kitchen in memory
-        kitchenElem.setAttribute('obj-model',{obj:'/assets/models/environment_v05.obj'});
+        kitchenElem.setAttribute('obj-model',{obj:'/assets/models/environment/building_final.obj'});
         kitchenElem.setAttribute('position',{x:0, y:1, z:0});
-        kitchenElem.setAttribute('material',{src:'/assets/textures/environment_v05.mtl'});
-        
+        kitchenElem.setAttribute('material',{src:'/assets/models/environment/floor_v02.png'});
+
     },
     createAvocado : function(){
         const Context_AF = this;
 
         let avocadoElem  = document.createElement('a-entity');//create new element avocado in memory
-        avocadoElem.setAttribute('obj-model',{obj:'/assets/models/avacado.obj'});
+        avocadoElem.setAttribute('obj-model',{obj:'/assets/models/props/avacado/avacado.obj'});
         avocadoElem.setAttribute('position',{x:0, y:0, z:0});
-        avocadoElem.setAttribute('material',{src:'/assets/textures/avacado.mtl'});
-        
+        avocadoElem.setAttribute('material',{src:'/assets/models/props/avacado/avocado.png'});
+
     },
     createSalmon : function(){
         const Context_AF = this;
@@ -35,16 +35,16 @@ AFRAME.registerComponent('load-environment', {
         salmonElem.setAttribute('obj-model',{obj:'/assets/models/salmon.obj'});
         salmonElem.setAttribute('position',{x:0, y:0, z:0});
         salmonElem.setAttribute('material',{src:'/assets/textures/salmon.png'});
-        
+
     },
     createSalt : function(){
         const Context_AF = this;
 
         let saltElem  = document.createElement('a-obj-model');//create new element salt in memory
-        saltElem.setAttribute('obj-model',{obj:'/assets/models/exports/export/salt/salt.obj'});
+        saltElem.setAttribute('obj-model',{obj:'/assets/models/props/salt/salt.obj'});
         saltElem.setAttribute('position',{x:0, y:0, z:0});
-        saltElem.setAttribute('material',{src:'/assets/models/exports/export/salt/salt.mtl'});
+        saltElem.setAttribute('material',{src:'/assets/models/props/salt/salt.mtl'});
 
-        
+
     }
 });
