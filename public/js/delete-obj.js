4e00d5947c0b99386f16ce7f13d8473c9821bf21
Nerly <nerlysaintFleur@cmail.carleton.ca>
2019-03-20 10:34:14 -0400

    Update delete-obj.js

--- public/js/delete-obj.js
+++ public/js/delete-obj.js
@@ -4,14 +4,14 @@ AFRAME.registerComponent('delete-obj', {
     schema : {},
     init :function(){
         const Context_AF = this;
-        
+
         Context_AF.el.addEventListener('click',function(event){
             console.log('click');
             Context_AF.deleteMyself();
         });
     },
     deleteMyself : function(){
-        //lets create and make the world a better place
+        //delete the obj
         const Context_AF = this;
         Context_AF.el.parentNode.removeChild(Context_AF.el);
     }
