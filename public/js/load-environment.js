42ae9b3944bb09acdbf896f9aad96346c487480c
The Flowers <TheFlowers1@Thes-iMac.local>
2019-03-27 00:58:04 -0400

    Update load-environment.js

--- public/js/load-environment.js
+++ public/js/load-environment.js
@@ -4,6 +4,7 @@ AFRAME.registerComponent('load-environment', {
     schema : {},
     init :function(){
         const Context_AF = this;
+
         Context_AF.createKitchen();
         Context_AF.createAvocado();
         Context_AF.createSalmon();
@@ -19,35 +20,7 @@ AFRAME.registerComponent('load-environment', {
         kitchenElem.setAttribute('position',{x:0, y:1, z:0});
         kitchenElem.setAttribute('material',{src:'/assets/models/environment/building_uv.png'});
 
-    },
-    createAvocado : function(){
-        const Context_AF = this;
-
-        let avocadoElem  = document.createElement('a-entity');//create new element avocado in memory
-        avocadoElem.setAttribute('obj-model',{obj:'/assets/models/props/avacado/avacado.obj'});
-        avocadoElem.setAttribute('position',{x:0, y:0, z:0});
-        avocadoElem.setAttribute('material',{src:'/assets/models/props/avacado/avocado.png'});
-        avocadoElem.setAttribute('delete-obj',{});
-
-
-    },
-    createSalmon : function(){
-        const Context_AF = this;
-
-        let salmonElem  = document.createElement('a-entity');//create new element salmon in memory
-        salmonElem.setAttribute('obj-model',{obj:'/assets/models/salmon.obj'});
-        salmonElem.setAttribute('position',{x:0, y:0, z:0});
-        salmonElem.setAttribute('material',{src:'/assets/textures/salmon.png'});
-
-    },
-    createSalt : function(){
-        const Context_AF = this;
-
-        let saltElem  = document.createElement('a-obj-model');//create new element salt in memory
-        saltElem.setAttribute('obj-model',{obj:'/assets/models/props/salt/salt.obj'});
-        saltElem.setAttribute('position',{x:0, y:0, z:0});
-        saltElem.setAttribute('material',{src:'/assets/models/props/salt/salt.mtl'});
-
+        const scene = document.querySelector('a-scene');
 
     }
 });
