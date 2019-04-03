8492f7b48601f8dbdb9b03df34f93650a1cbc64d
Nerly <nerlysaintFleur@cmail.carleton.ca>
2019-04-03 00:23:47 -0400

    new model

--- public/js/load-props.js
+++ public/js/load-props.js
@@ -39,10 +39,11 @@ AFRAME.registerComponent('load-props', {
         //relocate to the bowl area
 
         let avocadoElem  = document.createElement('a-entity');//create new element avocado in memory
-        avocadoElem.setAttribute('obj-model',{obj:'/assets/models/props/avacado/avacado.obj'});
-        avocadoElem.setAttribute('position',{x:0, y:0, z:0});
-        avocadoElem.setAttribute('scale',{x:1, y:1, z:1});
-        avocadoElem.setAttribute('material',{src:'/assets/models/props/avacado/avocado.png'});
+        avocadoInventory.setAttribute('id','relocateAvocado');
+        avocadoElem.setAttribute('obj-model',{obj:'/assets/models/kitchenEnviro/props/avocado/avocado.obj'});
+        avocadoElem.setAttribute('position',{x:1, y:1, z:1});
+        avocadoElem.setAttribute('scale',{x:0.5, y:0.5, z:0.5});
+        avocadoElem.setAttribute('material',{src:'/assets/models/kitchenEnviro/props/avocado/avocado.png'});
 
         const scene = document.querySelector('a-scene');
         scene.appendChild(avocadoElem);                     //append avodado to kitchen
@@ -51,10 +52,10 @@ AFRAME.registerComponent('load-props', {
               setTimeout(function(){ alert('Avocado Restocked' );
               let avocadoInventory  = document.createElement('a-entity');//create new element rice in memory
               avocadoInventory.setAttribute('id','avocado');
-              avocadoInventory.setAttribute('obj-model',{obj:'/assets/models/props/avacado/avacado.obj'});
-              avocadoInventory.setAttribute('position',{x:57, y:22.5, z:25});
-              avocadoInventory.setAttribute('scale',{x:1, y:1, z:1});
-              avocadoInventory.setAttribute('material',{src:'/assets/models/props/avacado/avocado.png'});
+              avocadoInventory.setAttribute('obj-model',{obj:'/assets/models/kitchenEnviro/props/avocado/avocado.obj'});
+              avocadoInventory.setAttribute('position',{x:0, y:0, z:0});
+              avocadoInventory.setAttribute('scale',{x:0.5, y:0.5, z:0.5});
+              avocadoInventory.setAttribute('material',{src:'/assets/models/kitchenEnviro/props/avocado/avocado.png'});
               avocadoInventory.setAttribute('load-props',{});
 
 
@@ -78,10 +79,11 @@ AFRAME.registerComponent('load-props', {
         //relocate to the bowl area
 
         let riceElem  = document.createElement('a-entity');//create new element rice in memory
-        riceElem.setAttribute('obj-model',{obj:'/assets/models/props/rice/rice.obj'});
-        riceElem.setAttribute('position',{x:0, y:0, z:0});
-        riceElem.setAttribute('scale',{x:1, y:1, z:1});
-        riceElem.setAttribute('material',{src:'/assets/models/props/rice/rice.png'});
+        avocadoInventory.setAttribute('id','relocateRice');
+        riceElem.setAttribute('obj-model',{obj:'/assets/models/kitchenEnviro/props/rice/rice.obj'});
+        riceElem.setAttribute('position',{x:1, y:1, z:1});
+        riceElem.setAttribute('scale',{x:0.5, y:0.5, z:0.5});
+        riceElem.setAttribute('material',{src:'/assets/models/kitchenEnviro/props/rice/rice.png'});
 
         const scene = document.querySelector('a-scene');
         scene.appendChild(riceElem);                     //append rice to kitchen
@@ -89,10 +91,10 @@ AFRAME.registerComponent('load-props', {
             setTimeout(function(){ alert('Rice Restocked' );
             let riceInventory  = document.createElement('a-entity');//create new element rice in memory
             riceInventory.setAttribute('id','rice');
-            riceInventory.setAttribute('obj-model',{obj:'/assets/models/props/rice/rice.obj'});
-            riceInventory.setAttribute('position',{x:52, y:22.5, z:-10});
-            riceInventory.setAttribute('scale',{x:1, y:1, z:1});
-            riceInventory.setAttribute('material',{src:'/assets/models/props/rice/rice.png'});
+            riceInventory.setAttribute('obj-model',{obj:'/assets/models/kitchenEnviro/props/rice/rice.obj'});
+            riceInventory.setAttribute('position',{x:0, y:0, z:0});
+            riceInventory.setAttribute('scale',{x:0.5, y:0.5, z:0.5});
+            riceInventory.setAttribute('material',{src:'/assets/models/kitchenEnviro/props/rice/rice.png'});
             riceInventory.setAttribute('load-props',{});
 
 
