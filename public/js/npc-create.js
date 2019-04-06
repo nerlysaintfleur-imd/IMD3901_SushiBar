7e097f6d754f926533dd07152512b2dca3ee6547
Stay In Geek <ijeomaofodire@hotmail.com>
2019-04-06 14:41:36 -0400

    Add files via upload

---
+++ public/js/npc-create.js
@@ -0,0 +1,37 @@
+'use strict';
+
+AFRAME.registerComponent('npc-create', {
+    schema : {},
+    init :function(){
+        const Context_AF = this;
+
+
+        document.querySelector('#bar').addEventListener('click', function() {
+            console.log('click');
+            Context_AF.deleteNPC();
+        })
+
+  
+
+
+    },
+
+    deleteNPC : function(){
+        let npc = document.querySelector('#npc');
+        npc.setAttribute('visible', false);
+        //npc.parentNode.removeChild(npc);
+
+    },
+
+    createNPC : function(){
+        let npc = document.querySelector('#npc');
+        npc.parentNode.removeChild(npc);
+
+    },
+
+   
+   
+
+});
+
+//if the elements are all at the same location try to serve button and then see
