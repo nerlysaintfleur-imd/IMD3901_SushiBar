d02f7f79b1cb4ab9a52634939a2509202b1a54ff
Nerly <nerlysaintFleur@cmail.carleton.ca>
2019-04-10 05:21:14 -0400

    Update npc-create.js

--- public/js/npc-create.js
+++ public/js/npc-create.js
@@ -9,27 +9,42 @@ AFRAME.registerComponent('npc-create', {
         document.querySelector('#bar').addEventListener('click', function() {
             console.log('click');
             Context_AF.deleteNPC();
-        })
+        }),
 
-  
+        deleteNPC : function(){
+               let npc = document.querySelector('#npc');
+               //npc.setAttribute('visible', false);
+               //npc.parentNode.removeChild(npc);
+               npc.parentNode.removeChild(npc);
+               console.log('NPC GONE');
 
 
-    },
+               var min=0;
+               var max=2000;
+               var random = Math.random() * (+max - +min) + +min;
 
-    deleteNPC : function(){
-        let npc = document.querySelector('#npc');
-        npc.setAttribute('visible', false);
-        //npc.parentNode.removeChild(npc);
+               setTimeout(function(){ alert('New Customer' );
+               let npcInventory  = document.createElement('a-entity');//create new element seaweed in memory
+               npcInventory.setAttribute('id','npc');
+               npcInventory.setAttribute('obj-model',{obj:'/assets/models/npc/Male1/male1.obj'});
+               npcInventory.setAttribute('position',{x:-51.837, y:0, z:0});
+               npcInventory.setAttribute('rotation',{x:0, y:90.0, z:0});
+               npcInventory.setAttribute('scale',{x:0.3, y:0.3, z:0.3});
+               npcInventory.setAttribute('material',{src:'/assets/models/npc/Male1/final_texture.png'});
+               npcInventory.setAttribute('npc-create',{});
 
-    },
+               const scene = document.querySelector('a-scene');
+               scene.appendChild(npcInventory);                     //append seaweed to kitchen
 
-    createNPC : function(){
-        let npc = document.querySelector('#npc');
-        npc.parentNode.removeChild(npc);
+               let hudBoardUpdate = document.querySelector('#hudBoard');
+               hudBoardUpdate.setAttribute('material',{src:'/assets/models/kitchenEnviro/HUD/board_avo.png'});
 
-    },
+               //Recharge sound
+               this.dingSound = document.querySelector('#ding');
+               this.dingSound.components['sound'].playSound();
 
-   
-   
+           //Delay creation time to remake it
+         }, random);
+
 
 });
